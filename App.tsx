96dd428e2d0d22f75b3af4ac3bf6af42ede7eb8c
Gui089 <guilhermesilba811@gmail.com>
2024-04-25 08:45:54 -0300

    Lib SafeArea Context add, para resolver o problema com espassamento no Topo

--- App.tsx
+++ App.tsx
@@ -9,14 +9,17 @@ import React from 'react';
 import { ThemeProvider} from '@shopify/restyle';
 import { theme } from './src/theme/Theme';
 import { LoginScreen } from './src/screens/auth/LoginScreen/LoginScreen';
+import { SafeAreaProvider } from 'react-native-safe-area-context';
 
 
 function App(): JSX.Element {
 
   return (
-    <ThemeProvider theme={theme}>
-      <LoginScreen />
-    </ThemeProvider>
+    <SafeAreaProvider>
+      <ThemeProvider theme={theme}>
+        <LoginScreen />
+      </ThemeProvider>
+    </SafeAreaProvider>
   );
 }
 
--- src/Components/Screen/Screen.tsx
+++ src/Components/Screen/Screen.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { Box } from "../Box/Box";
+import { useAppSafeArea } from "../../hooks/useAppSafeArea";
 
 interface Props {
     children: React.ReactNode;
 }
 
 export function ScreenComponent({children}: Props) {
+
+    const {top} = useAppSafeArea();
+
     return (
-        <Box paddingHorizontal="s24">
+        <Box paddingHorizontal="s24" style={{padding: top}}>
             {children}
         </Box>
     )
