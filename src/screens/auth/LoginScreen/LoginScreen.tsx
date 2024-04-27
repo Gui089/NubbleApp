e77a75c44516f6a63885774f3bc792ea12dd11f7
Gui089 <guilhermesilba811@gmail.com>
2024-04-27 14:19:29 -0300

    Add primeira navegacao para da pagina de login para a de SignUp

--- src/screens/auth/LoginScreen/LoginScreen.tsx
+++ src/screens/auth/LoginScreen/LoginScreen.tsx
@@ -7,10 +7,15 @@ import { Icon } from "../../../Components/icon/Icon";
 import { Button } from "../../../Components/Button/Button";
 import { ScreenComponent } from "../../../Components/Screen/Screen";
 
-export function LoginScreen() {
+export function LoginScreen({navigation}) {
+
+  const navigateToSignUpScreen = () => {
+    navigation.navigate('SignUpScreen');
+  }
+
     return (
         <ScreenComponent>
-            <SafeAreaView>
+        <SafeAreaView >
         <View style={{paddingHorizontal: 16}}>
             <Text marginBottom='s8' preset='headingLarge'>Olá!</Text>
             <Text marginBottom='s40' preset='paragraphLarge'>Digite seu e-mail e senha para entrar</Text>
@@ -20,7 +25,7 @@ export function LoginScreen() {
               errorMessage='Mensagem de erro'
               label='Email'
               placeholder='Digite seu e-mail' 
-             />
+            />
           </Box>
 
           <Box marginBottom='s20'>
@@ -34,7 +39,7 @@ export function LoginScreen() {
             <Text bold marginTop='s10' color='primary' preset='paragraphSmall'>Esqueci minhan senha</Text>
 
             <Button marginTop='s48' title='Entrar' />
-            <Button preset='outline' marginTop='s12' title='Criar uma conta'/>
+            <Button onPress={navigateToSignUpScreen} preset='outline' marginTop='s12' title='Criar uma conta'/>
         </View>
       </SafeAreaView>
         </ScreenComponent>
