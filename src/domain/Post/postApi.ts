ec0bad440737e7e8f49ecd990d85aecbb4494b9d
Gui089 <guilhermesilba811@gmail.com>
2024-05-09 19:55:45 -0300

    Camda de domain para conectar com a api

---
+++ src/domain/Post/postApi.ts
@@ -0,0 +1,11 @@
+import { postListMock } from "./postListMock";
+import { Post } from "./types";
+
+
+async function getList(): Promise<Post[]> {
+    return postListMock;
+}
+
+export const postApi = {
+    getList,
+};
