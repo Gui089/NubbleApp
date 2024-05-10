21d9f9e5d9dd016e3a0c46215c004cb258faa649
Gui089 <guilhermesilba811@gmail.com>
2024-05-10 08:17:23 -0300

    FlaList render data in HomeScreen

--- src/screens/app/HomeScreen/HomeScreen.tsx
+++ src/screens/app/HomeScreen/HomeScreen.tsx
@@ -1,13 +1,29 @@
 import React, { useEffect, useState } from "react";
 
-import { ScreenComponent, Text } from "@Components";
+import { Box, ScreenComponent, Text } from "@Components";
 import { AppTabScreenProps } from "src/routes/navigationTypes";
 import { Post, postService } from "@domain";
+import { FlatList } from "react-native-gesture-handler";
+import { Dimensions, Image, ListRenderItemInfo } from "react-native";
+
 
 
 export const HomeScreen = ({navigation}: AppTabScreenProps<'HomeScreen'>) => {
 
     const [posts, setPosts] = useState<Post[]>([]);
+
+    const renderItem = ({item}: ListRenderItemInfo<Post>) => {
+        return (
+            <Box mb="s24">
+                <Box flexDirection="row">
+                    <Image style={{width:32, height:32}} source={{uri: item.author.profileURL}}/>
+                    <Text>{item.author.userName}</Text>
+                </Box>
+                    <Image 
+                    style={{width: Dimensions.get('screen').width, height:300}} source={{uri: item.imageURL}}/>
+            </Box>
+        )
+    }
 
     useEffect(() => {
         postService.getList().then(( list ) => setPosts(list));
@@ -15,7 +31,11 @@ export const HomeScreen = ({navigation}: AppTabScreenProps<'HomeScreen'>) => {
 
     return (
         <ScreenComponent>
-            {posts.map(post => <Text>{post.text}</Text>)}
+            <FlatList 
+                data={posts}
+                keyExtractor={(item) => item.id}
+                renderItem={renderItem}
+                />
         </ScreenComponent>
     )
 }
