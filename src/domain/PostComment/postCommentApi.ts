6a09dc6479e9a22fecaf5ba2dd405fc08665a59f
Gui089 <guilhermesilba811@gmail.com>
2024-05-21 11:10:53 -0300

    Add domain PostComment and conection in API

---
+++ src/domain/PostComment/postCommentApi.ts
@@ -0,0 +1,18 @@
+import { PageAPI, PageParams } from "@api";
+import { api } from "@api";
+import { PostCommentAPI } from "./postCommentTypes";
+
+async function getList(post_id:number,pageParams?: PageParams): Promise<PageAPI<PostCommentAPI>> {
+    const response = await api.get<PageAPI<PostCommentAPI>>('user/post_comment', {
+        params: {
+            post_id: post_id,
+            ...pageParams,
+        }
+    });
+    
+    return response.data;
+}
+
+export const postCommentApi = {
+    getList,
+};
---
+++ src/domain/PostComment/postCommentService.ts
@@ -0,0 +1,20 @@
+import { Page } from "src/types/Post";
+
+import { apiAdapter } from "@api";
+import { PostComment } from "./postCommentTypes";
+import { postCommentApi } from "./postCommentApi";
+import { postCommentAdapter } from "./postCommentAdapter";
+
+
+async function getList(postId:number,page: number):Promise<Page<PostComment>> {
+    const postCommentPageApi = await postCommentApi.getList(postId, {page, per_page:10});
+
+    return {
+        data: postCommentPageApi.data.map(postCommentAdapter.toPostComment),
+        meta: apiAdapter.toMetaDataPage(postCommentPageApi.meta)
+    }
+}
+
+export const postService = {
+    getList,
+};
