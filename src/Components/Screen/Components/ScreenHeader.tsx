aa3f44d67e6e51cc58500b19e637d10787ddce4e
Gui089 <guilhermesilba811@gmail.com>
2024-05-22 10:46:19 -0300

    Ajuste no alinhamento do title

--- src/Components/Screen/Components/ScreenHeader.tsx
+++ src/Components/Screen/Components/ScreenHeader.tsx
@@ -9,20 +9,25 @@ export const ScreenHeader = ({title, changeGoBack}: Props) => {
     const navigation = useNavigation();
 
     return (
-        <Box>
+        <Box 
+            flexDirection="row" 
+            justifyContent="space-between"
+            alignItems="center"
+            mb="s24"
+            >
             {changeGoBack && 
                     <TouchableOpacityBox 
                         onPress={navigation.goBack} 
-                        flexDirection="row" 
-                        justifyContent="space-between"
-                        alignItems="center"
-                        mb="s40"
+                        flexDirection="row"
                         >
                         <Icon name="arrowLeft" color="primary"/>
-                        {!title && <Text preset="paragraphMedium" ml="s8" semiBold>Voltar</Text>}
+                        {!title && <Text preset="paragraphMedium" ml="s8" semiBold>
+                            Voltar
+                        </Text>}
+                    </TouchableOpacityBox>
+                    }
                         {title && <Text preset="headingSmall">{title}</Text>}
                         {title && <Box width={20} />}
-            </TouchableOpacityBox>}
         </Box>
     )
 }
