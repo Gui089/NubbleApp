1b6b24c08bd90273d7760a247ccc8422ddc32fa4
Gui089 <guilhermesilba811@gmail.com>
2024-06-05 09:37:16 -0300

    :white_check_mark: Add: staleTime for cache in ProfileScreen

--- src/domain/User/useCases/useUserGetById.ts
+++ src/domain/User/useCases/useUserGetById.ts
@@ -5,28 +5,11 @@ import { QueryKeys } from "@infra";
 
 
 export function userUserGetById(id:number) {
-    /* const [user, setUser] = useState<User>();
-    const [error, setError] = useState<boolean | null>(null);
-    const [loading, setLoading] = useState(false);
-
-    const getUserById = useCallback(async () => {
-        try {
-            const _user = await userService.getById(id);
-            setUser(_user);
-        } catch (err) {
-            setError(true);
-        } finally {
-            setLoading(false);
-        }
-    }, [id]);
-
-    useEffect(() => {
-        getUserById();
-    }, [getUserById]); */
 
     const {data, isLoading, isError} = useQuery({
         queryKey: [QueryKeys.UserGetById],
         queryFn: () => userService.getById(id),
+        staleTime: 1000 * 10,
     });
 
     return {
